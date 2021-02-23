6b4936604af1818cefd2b969c3c5ef17f4b1af6a
Federico Poli <fapoli@gmail.com>
2021-02-23 14:34:21 -0300

    initial commit with fadein/out animation

---
+++ Scripts/ScreenTransitions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenTransitions : MonoBehaviour
+{
+    public static ScreenTransitions instance { get; private set; }
+
+    public List<TransitionAnimator> animators;
+
+    private Dictionary<string, Animator> animatorsByName = new Dictionary<string, Animator>();
+
+    private void Start()
+    {
+        if (!instance)
+            return;
+
+        foreach (var animator in animators)
+            animatorsByName[animator.name] = animator.animator;
+
+        instance = this;
+    }
+
+    public void PlayStart(string name)
+    {
+        if (animatorsByName.ContainsKey(name))
+            animatorsByName[name].SetTrigger("Start");
+    }
+
+    public void PlayEnd(string name)
+    {
+        if (animatorsByName.ContainsKey(name))
+            animatorsByName[name].SetTrigger("End");
+    }
+}
+
+[System.Serializable]
+public class TransitionAnimator
+{
+    public string name;
+    public Animator animator;
+}
